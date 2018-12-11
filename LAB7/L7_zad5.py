1dc44c96b939eb5f49d7891e96e5d9e27037e611
Piotr Kolebski <piotrkolebski@gmail.com>
2018-12-11 19:58:22 +0100

    resize cardioid line with keybord

--- LAB7/L7_zad5.py
+++ LAB7/L7_zad5.py
@@ -7,14 +7,29 @@ shad = None
 ures = None 
 winwidht = None 
 winheight = None
+line_size = 1
+line_size_location = None
+
+def keyboard(key, x, y):
+    global line_size
+    ch = key.decode("utf-8")
+    if ch == 'a':
+        line_size *= 2
+        return
+    elif ch == 's':
+        line_size *= 0.5
+        return
+    line_size = int(ch)
+
 
 def paint():
-    global shad, ures, winwidht, winheight
+    global shad, ures, winwidht, winheight, line_size, line_size_location
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
     glTranslatef(0.0, -0.0, -4.0)
     glUseProgram(shad)
     glUniform2f(ures, winwidth//2, winheight//2)
+    glUniform1f(line_size_location, line_size)
     glBegin(GL_POLYGON)
     glVertex2f(-5.0, -2.0)
     glVertex2f(5.0, -2.0)
@@ -24,7 +39,7 @@ def paint():
     # glutSolidSphere(1.0, 32, 32)
     glutSwapBuffers()
 def main():
-    global shad, ures, winwidth, winheight
+    global shad, ures, winwidth, winheight, line_size, line_size_location
     glutInit()
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
     winwidth = glutGet(GLUT_SCREEN_WIDTH)
@@ -37,23 +52,24 @@ def main():
     glLoadIdentity()
     gluPerspective(50.0, float(winwidth/winheight), 0.1, 100.0)
     glutDisplayFunc(paint)
+    glutIdleFunc(paint)
+    glutKeyboardFunc(keyboard)
     glMatrixMode(GL_MODELVIEW)
     shad = compileProgram(
     compileShader("""
     #extension GL_EXT_gpu_shader4 : enable
     uniform vec2 ures;
+    uniform float line_size;
     
     float getf(vec2 st) {
     float x = st.x * 20 - 10;
     float y = st.y * 20 - 10;
     float a = 2;
-        if (abs(pow((pow(x, 2.) + pow(y,2.)), 2.) + 4*a*x*(pow(x,2.)+pow(y, 2.))-4*pow(a,2.) *pow(y,2.)) < 4.) {
+        if (abs(pow((pow(x, 2.) + pow(y, 2.)), 2.) + 4. * a * x * (pow(x, 2.) + pow(y, 2.)) - 4. * pow(a, 2.) * pow(y, 2.)) < line_size) {
             return 1.0;
         }
         return 0.0;
-    
-
-    
+       
     }
     void main() {
         vec2 st = gl_FragCoord.xy/ures;
@@ -61,6 +77,7 @@ def main():
         gl_FragColor = vec4(val, 20., 20., 20.0);
     } """, GL_FRAGMENT_SHADER), )
     ures = glGetUniformLocation(shad, "ures")
+    line_size_location = glGetUniformLocation(shad, "line_size")
     print(ures)
     glutMainLoop()
 main()
