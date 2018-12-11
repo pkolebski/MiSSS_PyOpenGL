fcc381fb35b1c96fd0ed8b32dd90be61629bfbb9
Piotr Kolebski <piotrkolebski@gmail.com>
2018-12-11 19:24:44 +0100

    drawind cardioid

--- LAB7/L7_zad5.py
+++ LAB7/L7_zad5.py
@@ -38,23 +38,30 @@ def main():
     gluPerspective(50.0, float(winwidth/winheight), 0.1, 100.0)
     glutDisplayFunc(paint)
     glMatrixMode(GL_MODELVIEW)
-    # (x**2 + y**2 - a*x)**2 = (a**2) * (x**2 + y**2)
     shad = compileProgram(
     compileShader("""
+    #extension GL_EXT_gpu_shader4 : enable
     uniform vec2 ures;
     
     float getf(vec2 st) {
-        return (((int(st.x)^2) + (int(st.y)^2))^2) + 4 * 1 * int(st.x) * ((int(st.x)^2) + (int(st.y)^2)) - 4*(1^2)*(int(st.y)^2);
-        
+    float x = st.x * 20 - 10;
+    float y = st.y * 20 - 10;
+    float a = 2;
+        if (abs(pow((pow(x, 2.) + pow(y,2.)), 2.) + 4*a*x*(pow(x,2.)+pow(y, 2.))-4*pow(a,2.) *pow(y,2.)) < 4.) {
+            return 1.0;
+        }
+        return 0.0;
+    
+
+    
     }
     void main() {
-        vec2 st = gl_FragCoord.xy/ures ;
-        float val = getf(st) ;
-        gl_FragColor = vec4(val, 1.0, 1.0, 1.0) ;
+        vec2 st = gl_FragCoord.xy/ures;
+        float val = getf(st);
+        gl_FragColor = vec4(val, 20., 20., 20.0);
     } """, GL_FRAGMENT_SHADER), )
     ures = glGetUniformLocation(shad, "ures")
     print(ures)
     glutMainLoop()
 main()
 
-# (((int(st.x)^2) + (int(st.y)^2))^2) + 4 * 1 * int(st.x) * ((int(st.x)^2) + (int(st.y)^2)) - 4*(1^2)*(int(st.y)^2)
